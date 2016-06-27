c271a2795e7176a7d8c1e09fb72bda29bbad9148
Sriram <Sriram@Sriram-PC>
2016-06-27 20:28:11 +0530

    Palindrome program added

---
+++ 00-core-java-programs/src/com/msrm/basicprograms/Palindrome.java
@@ -0,0 +1,41 @@
+package com.msrm.basicprograms;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		String str = "MADAM";
+		Palindrome p = new Palindrome();
+		if (p.isPalindrome(str)) {
+			// if (p.palindrome(str.toCharArray(), 0, str.length() - 1)) {
+			System.out.println(str + " is Palindrome");
+		} else {
+			System.out.println(str + " not is Palindrome");
+		}
+	}
+
+	public boolean palindrome(char[] ch, int start, int end) {
+		if (start < end) {
+			if (ch[start] != ch[end]) {
+				return false;
+			}
+		} else {
+			return true;
+		}
+		return palindrome(ch, ++start, --end);
+	}
+
+	public boolean isPalindrome(String str) {
+		if (str.length() % 2 != 1) {
+			return false;
+		}
+
+		int lastIndex = str.length() - 1;
+		for (int i = 0; i < str.length() / 2; i++) {
+			if (str.charAt(0) != str.charAt(lastIndex))
+				return false;
+		}
+
+		return true;
+	}
+
+}
