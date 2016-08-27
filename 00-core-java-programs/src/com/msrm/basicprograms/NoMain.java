7ca8cce5700a895bdda2d2ee641f0585e7d91f23
srirammuthaiah <srirammuthaiah@192.168.1.12>
2016-08-27 10:03:45 +0530

    Execute statement without main() method in java

---
+++ 00-core-java-programs/src/com/msrm/basicprograms/NoMain.java
@@ -0,0 +1,18 @@
+package com.msrm.basicprograms;
+
+/**
+ * Print "Hello" message without calling main methodS
+ * To run the below program in command line
+ * $ java -Djava.system.class.loader=NoMain
+ * @author srirammuthaiah
+ *
+ */
+public class NoMain extends ClassLoader {
+
+	public NoMain(ClassLoader cl) {
+		super(cl);
+		System.out.println("Hello");
+		System.exit(0);
+	}
+	
+}
