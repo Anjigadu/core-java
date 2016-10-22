921019051b454c00817863aaf2c5ec66853ddff6
srirambtechit <srirambtecit@gmail.com>
2016-10-22 22:54:49 +0530

    Array pairing and merging program finished

--- hands-on/src/com/msrm/handson/algorithms/ArrayPairingApp.java
+++ hands-on/src/com/msrm/handson/algorithms/ArrayPairingApp.java
@@ -1,5 +1,8 @@
 package com.msrm.handson.algorithms;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Given an array A[] and a number x, check for pair in A[] with sum as x
  * 
@@ -9,8 +12,23 @@ package com.msrm.handson.algorithms;
 public class ArrayPairingApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] array = { 4, 3, 2, 0, 8, 9, 1, 5, 0 };
+		int x = 5;
+		ArrayPairingApp app = new ArrayPairingApp();
+		List<String> pairs = app.pairs(array, x);
+		System.out.println(pairs);
+	}
 
+	public List<String> pairs(int[] array, int x) {
+		List<String> pairs = new ArrayList<>();
+		for (int i = 0; i < array.length; i++) {
+			for (int j = i + 1; j < array.length; j++) {
+				if (array[i] + array[j] == x) {
+					pairs.add("(" + array[i] + "," + array[j] + ")");
+				}
+			}
+		}
+		return pairs;
 	}
 
 }
--- hands-on/src/com/msrm/handson/algorithms/MergeArrayApp.java
+++ hands-on/src/com/msrm/handson/algorithms/MergeArrayApp.java
@@ -15,28 +15,113 @@ public class MergeArrayApp {
 		MergeArrayApp app = new MergeArrayApp();
 
 		// Assumption: note that below 2 arrays should be sorted in nature
-		int[] arrayOne = { 1, 2, 5, 9, 10, 14, 18, 49, 75 };
-		int[] arrayTwo = { 2, 4, 6, 9, 14, 23, 49, 83, 98, 103, 493, 493 };
-		int[] newArray = app.mergeSortedArray(arrayOne, arrayTwo);
+		int[] arrayOne = { 1, 3, 5, 9, 10, 14, 18, 49, 75 };
+		int[] arrayTwo = { 2, 4, 6, 8, 14, 23, 49, 83, 98 };
+//		 int [] arrayTwo = { 2, 4, 6, 9, 14, 23, 49, 83, 98, 103, 493, 493 };
+		int[] newArray = app.mergeSortedArraySolutionTwo(arrayOne, arrayTwo);
 
 		System.out.println("Array one: " + Arrays.toString(arrayOne));
 		System.out.println("Array two: " + Arrays.toString(arrayTwo));
 		System.out.println("New array: " + Arrays.toString(newArray));
 	}
 
-	public int[] mergeSortedArray(int[] arrayOne, int[] arrayTwo) {
-		
-		int[] tmpArray = new int[arrayOne.length + arrayTwo.length];
+	private class Result {
+		int[] array;
+		int index;
+		int dups;
 
-		int iterateTill = arrayOne.length > arrayTwo.length ? arrayOne.length : arrayTwo.length;
-		int elementCount = 0;
-
-		for (int i = 0; i < iterateTill; i++) {
-			
+		public int[] getResult() {
+			int[] result = new int[array.length - dups];
+			System.arraycopy(array, 0, result, 0, array.length - dups);
+			return result;
 		}
 
-		int[] resultArray = null;
-		return resultArray;
+	}
+
+	public int[] mergeSortedArraySolutionTwo(int[] arrayOne, int[] arrayTwo) {
+		int size = arrayOne.length + arrayTwo.length;
+		Result result = new Result();
+		result.array = new int[size];
+		result.index = 0;
+		int len = arrayOne.length > arrayTwo.length ? arrayOne.length : arrayTwo.length;
+
+		for (int i = 0; i < len; i++) {
+			if (i < arrayOne.length) {
+				fillBuffer(result, arrayOne, i);
+			}
+			if (i < arrayTwo.length) {
+				fillBuffer(result, arrayTwo, i);
+			}
+		}
+		return result.getResult();
+	}
+
+	private void fillBuffer(Result result, int[] array, int arrayIndex) {
+		if (result.index >= 1) {
+			if (result.array[result.index - 1] > array[arrayIndex]) {
+				result.array[result.index] = result.array[result.index - 1];
+				result.array[result.index - 1] = array[arrayIndex];
+				result.index++;
+			} else if (result.array[result.index - 1] < array[arrayIndex]) {
+				result.array[result.index++] = array[arrayIndex];
+			} else {
+				result.dups++;
+			}
+		} else {
+			// buffer has no data, only one time this block executes
+			result.array[result.index++] = array[arrayIndex];
+		}
+	}
+
+	// This implementation would have redundant codes
+	public int[] mergeSortedArraySolutionOne(int[] arrayOne, int[] arrayTwo) {
+
+		int size = arrayOne.length + arrayTwo.length;
+		int[] buffer = new int[size];
+		int bufferIndex = 0;
+		int dups = 0;
+		int len = arrayOne.length > arrayTwo.length ? arrayOne.length : arrayTwo.length;
+
+		for (int i = 0; i < len; i++) {
+
+			if (i < arrayOne.length) {
+				// buffer has already data in it
+				if (bufferIndex >= 1) {
+					if (buffer[bufferIndex - 1] > arrayOne[i]) {
+						buffer[bufferIndex] = buffer[bufferIndex - 1];
+						buffer[bufferIndex - 1] = arrayOne[i];
+						bufferIndex++;
+					} else if (buffer[bufferIndex - 1] < arrayOne[i]) {
+						buffer[bufferIndex++] = arrayOne[i];
+					} else {
+						dups++;
+					}
+				} else {
+					// buffer has no data, only one time this block executes
+					buffer[bufferIndex++] = arrayOne[i];
+				}
+			}
+
+			if (i < arrayTwo.length) {
+				if (bufferIndex >= 1) {
+					if (buffer[bufferIndex - 1] > arrayTwo[i]) {
+						buffer[bufferIndex] = buffer[bufferIndex - 1];
+						buffer[bufferIndex - 1] = arrayTwo[i];
+						bufferIndex++;
+					} else if (buffer[bufferIndex - 1] < arrayTwo[i]) {
+						buffer[bufferIndex++] = arrayTwo[i];
+					} else {
+						dups++;
+					}
+				} else {
+					buffer[bufferIndex++] = arrayTwo[i];
+				}
+			}
+		}
+
+		int[] result = new int[buffer.length - dups];
+		System.arraycopy(buffer, 0, result, 0, buffer.length - dups);
+		return result;
 	}
 
 }
