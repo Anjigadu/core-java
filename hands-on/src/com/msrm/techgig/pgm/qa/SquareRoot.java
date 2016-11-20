92f0f4c7771f573356757bcb348e1dba5e5fd9cb
srirambtechit <srirambtecit@gmail.com>
2016-11-21 01:12:09 +0530

    SquareRoot of number program

---
+++ hands-on/src/com/msrm/techgig/pgm/qa/SquareRoot.java
@@ -0,0 +1,19 @@
+package com.msrm.techgig.pgm.qa;
+
+public class SqureRoot {
+
+	public static void main(String[] args) {
+		double result = sqrt(Double.parseDouble(args[0]));
+		System.out.println("result : " + result);
+	}
+
+	public static double sqrt(double number) {
+		double error = 1e-3;
+		double s = number;
+		while((s - number / s) > error) {
+			s = (s + number / s) / 2;
+		}
+		return s;
+	}
+
+}
